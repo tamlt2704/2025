1407a19d59f3b29aa9f5787da104c19d20656cc3
tamlt2704 <tamlt2704@gmail.com>
2024-12-16 12:25:16 +0800

    validation error

--- LearnReactWithTypeScript/ch4-01/src/pages/ContactReactHookForm.tsx
+++ LearnReactWithTypeScript/ch4-01/src/pages/ContactReactHookForm.tsx
@@ -1,7 +1,8 @@
 import { clearConfigCache } from 'prettier';
 import React, { FormEvent, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, FieldError } from 'react-hook-form';
 import { Form, ActionFunctionArgs, redirect, useNavigate } from 'react-router-dom';
+import ValidationError from '../ValidationError';
 
 type Contact = {
   name: string;
@@ -12,54 +13,68 @@ type Contact = {
 const fieldStyle = 'flex flex-col mb-2';
 
 export default function ContactReactHookForm() {
-  const { register, handleSubmit } = useForm<Contact>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Contact>();
   const navigate = useNavigate();
   function onSubmit(contact: Contact) {
     console.log('submitted contact', contact);
     navigate(`/thankyou/${contact.name}`);
   }
+
+  function getEditorStyle(fieldError: FieldError | undefined) {
+    console.log(fieldError);
+    const style = fieldError ? 'border-2 border-red-500' : 'border-2 border-black';
+    console.log(style);
+    return style;
+  }
   return (
     <div className="flex flex-col py-10 max-w-md mx-auto">
       <h2 className="text-3xl font-bold underline mb-3"> Contact us </h2>
       <p className="mb-3">If you enter your details we'll get back to you as soon as we can</p>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className={fieldStyle}>
           <label htmlFor="name"> Your name </label>
           <input
-            className="border-solid border-2 border-red-500"
+            className={getEditorStyle(errors.name)}
             type="text"
             id="name"
-            {...register('name')}
+            {...register('name', { required: 'You must enter your name' })}
           />
+          <ValidationError fieldError={errors.name} />
         </div>
 
         <div className={fieldStyle}>
           <label htmlFor="email"> Your Email </label>
           <input
-            className="border-solid border-2 border-red-500"
+            className={getEditorStyle(errors.email)}
             type="text"
             id="email"
-            {...register('email')}
+            {...register('email', { required: 'You must enter your email' })}
           />
+          <ValidationError fieldError={errors.email} />
         </div>
         <div className={fieldStyle}>
           <label htmlFor="reason"> Reason for contact </label>
           <select
-            className="border-solid border-2 border-red-500"
+            className={getEditorStyle(errors.reason)}
             id="reason"
-            {...register('reason')}
+            {...register('reason', { required: 'You must enter your reason' })}
           >
             <option value=""></option>
             <option value="support">Support</option>
             <option value="feedback">Feedback</option>
             <option value="other">Other</option>
           </select>
+          <ValidationError fieldError={errors.reason} />
         </div>
         <div className={fieldStyle}>
           <label htmlFor="notes">Additional notes</label>
           <textarea
             id="notes"
-            className="border-solid border-2 border-red-500"
+            className="border-solid border-2 border-black"
             {...register('notes')}
           />
         </div>
